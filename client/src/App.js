7ec98a87f00cfbf90efedf337c74b307f85cccb6
Manuel Murillo <mmpeira@gmail.com>
2023-03-04 20:42:29 +0100

    navbar casi terminado

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
+import NavBar from './components/Header/Navbar/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola desde React</h1>
-    </div>
+    <ChakraProvider>
+      <Router>
+      <NavBar/>
+      <Main />
+      <Footer/>
+      </Router>
+    </ChakraProvider>
   );
 }
 
