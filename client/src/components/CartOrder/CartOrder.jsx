988581f0962322c4b98bcef159514302f84a2979
Manuel Murillo <mmpeira@gmail.com>
2023-03-09 23:42:55 +0100

    quitados comentarios y archivos vacios

--- client/src/components/CartOrder/CartOrder.jsx
+++ client/src/components/CartOrder/CartOrder.jsx
@@ -5,20 +5,12 @@ import {
   Stack,
   Text,
 } from "@chakra-ui/react";
-import { useState } from "react";
 import { useSelector } from "react-redux";
-import { Link as ReactLink, useNavigate } from "react-router-dom";
+import { Link as ReactLink } from "react-router-dom";
 
 const CartOrder = () => {
-  const [buttonLoading, setButtonLoading] = useState();
   const cartItems = useSelector((state) => state.cart);
   const { subtotal } = cartItems;
-  const navigate = useNavigate;
-
-  const checkoutHandler = () => {
-    setButtonLoading(true);
-    navigate("/checkout");
-  };
 
   return (
     <Stack spacing="8" borderWidth="1px" rounded="lg" padding="8" w="full">
@@ -39,10 +31,7 @@ const CartOrder = () => {
         colorScheme="orange"
         size="lg"
         fontSize="md"
-        isLoading={buttonLoading}
-        onClick={() => checkoutHandler()}
-      >
-        Paypal
+      > Paypal
       </Button>
     </Stack>
   );
