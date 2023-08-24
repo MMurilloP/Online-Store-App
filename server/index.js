a97ea8569b5f5055d24dfc555a1069b6f06ddc6c
Manuel Murillo <mmpeira@gmail.com>
2023-08-24 18:03:40 +0200

    deploy10

--- server/index.js
+++ server/index.js
@@ -15,6 +15,14 @@ const port = process.env.PORT || 8001;
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+// Configuración para servir archivos estáticos en producción
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 app.listen(port, () =>
   console.log(`Servidor corriendo en ${port} http://localhost:${port}`)
 );
