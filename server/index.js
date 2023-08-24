503785ec01f77605be780478518355bd9227be51
Manuel Murillo <mmpeira@gmail.com>
2023-08-24 18:20:28 +0200

    deploy11

--- server/index.js
+++ server/index.js
@@ -17,9 +17,9 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // Configuración para servir archivos estáticos en producción
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.resolve(__dirname, "client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
   });
 }
 
