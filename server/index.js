0472974222ed27977340dfda0007b1e9f8adbc64
Manuel Murillo <mmpeira@gmail.com>
2023-08-24 17:34:46 +0200

    deploy7

--- server/index.js
+++ server/index.js
@@ -1,21 +1,26 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
 require("./utils/db");
 
-
-
-
-// Conexion db Mongo
-const port = process.env.PORT || 5001;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const productRouters = require('./routes/productRoutes');
+const productRouters = require("./routes/productRoutes");
 
+app.use("/api/products", productRouters);
 
-app.use('/api/products', productRouters);
+const port = process.env.PORT || 8001;
 
+// Deploy
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () =>
   console.log(`Servidor corriendo en ${port} http://localhost:${port}`)
